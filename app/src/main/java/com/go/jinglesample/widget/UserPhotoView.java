d5a365e879fa022d40cc543ca196a14fb3a00151
Gabor Orosz <gabor.orosz@me.com>
2017-07-24 21:50:08 +0200

    First step of UserPhotoView

---
+++ app/src/main/java/com/go/jinglesample/widget/UserPhotoView.java
@@ -0,0 +1,26 @@
+package com.go.jinglesample.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.FrameLayout;
+
+public class UserPhotoView extends FrameLayout {
+    public UserPhotoView(Context context) {
+        super(context);
+        init(context);
+    }
+
+    public UserPhotoView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+    }
+
+    public UserPhotoView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context);
+    }
+
+    private void init(final Context context) {
+        // TODO
+    }
+}
