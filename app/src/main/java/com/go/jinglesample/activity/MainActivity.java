d66e98219446d6e02d4f3ef71b51a59d31a60532
Gabor Orosz <gabor.orosz@me.com>
2017-07-24 21:45:37 +0200

    New project structure for java files (activity + widget)

--- app/src/main/java/com/go/jinglesample/activity/MainActivity.java
+++ app/src/main/java/com/go/jinglesample/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.go.jinglesample;
+package com.go.jinglesample.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.go.jinglesample.R;
 
 public class MainActivity extends AppCompatActivity {
 
--- app/src/main/java/com/go/jinglesample/widget/UserDetailsView.java
+++ app/src/main/java/com/go/jinglesample/widget/UserDetailsView.java
@@ -1,9 +1,11 @@
-package com.go.jinglesample;
+package com.go.jinglesample.widget;
 
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.ScrollView;
+
+import com.go.jinglesample.R;
 
 public class UserDetailsView extends ScrollView {
 
